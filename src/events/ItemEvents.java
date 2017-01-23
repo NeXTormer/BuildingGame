1d2a7b4b616db8310e4d00016d9342ff908f7506
GameZterr <bitzan1999@gmail.com>
2017-01-23 12:05:46 +0100

    Item ClickEvent

--- src/events/ItemEvents.java
+++ src/events/ItemEvents.java
@@ -1,8 +1,11 @@
 package events;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import game.Game;
 
@@ -18,5 +21,16 @@ public class ItemEvents implements Listener {
 	public void playerDropItemEvent(PlayerDropItemEvent e)
 	{
 		e.setCancelled(true);
+	}
+	
+	public void playerInteractEvent(PlayerInteractEvent e)
+	{
+		if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK)
+		{
+			if(e.getMaterial().equals(Material.PRISMARINE_SHARD))
+			{
+				
+			}
+		}
 	}
 }
