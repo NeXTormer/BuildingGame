1119b5317f952514f006f03bd9930855b05dbb5f
GameZterr <bitzan1999@gmail.com>
2017-03-01 16:39:59 +0100

    FreezeBrawl Nerf

--- src/Brawls/BrawlFreeze.java
+++ src/Brawls/BrawlFreeze.java
@@ -4,13 +4,13 @@ import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 import game.Game;
-import net.minecraft.server.v1_8_R3.Material;
 
 public class BrawlFreeze extends PlayerBrawl {
 
@@ -36,18 +36,20 @@ public class BrawlFreeze extends PlayerBrawl {
 				p.playSound(p.getLocation(), Sound.STEP_SNOW, 1, 1);
 				
 				Location replaceLoc = p.getLocation();
-				replaceLoc.setY(p.getLocation().getY()-3);
-				replaceLoc.setX(p.getLocation().getX()-3);
-				replaceLoc.setZ(p.getLocation().getZ()-3);
+				replaceLoc.setY(p.getLocation().getY()-1);
+				replaceLoc.setX(p.getLocation().getX()-2);
+				replaceLoc.setZ(p.getLocation().getZ()-2);
 			    World world = Bukkit.getWorld(game.locationCfg.getString("locations.lobby.world"));
 			    Location edgeMin = new Location(world, replaceLoc.getX(), replaceLoc.getY(), replaceLoc.getZ());
-			    Location edgeMax = new Location(world, replaceLoc.getX()+6, replaceLoc.getY()+6, replaceLoc.getZ()+6);
+			    Location edgeMax = new Location(world, replaceLoc.getX()+6, replaceLoc.getY()+4, replaceLoc.getZ()+4);
 			   
 			    for (int x = edgeMin.getBlockX(); x < edgeMax.getBlockX(); x ++) {
 			        for (int y = edgeMin.getBlockY(); y < edgeMax.getBlockY(); y ++) {
 			            for (int z = edgeMin.getBlockZ(); z < edgeMax.getBlockZ(); z ++) {
 			            	Location currentLocation = new Location(world, x, y, z);
-			            	if(currentLocation.getBlock().getType().equals(Material.AIR))
+			            	Location plotLocation = currentLocation;
+			            	plotLocation.setY(1);
+			            	if(p.getWorld().getBlockAt(currentLocation).getType() == Material.AIR && p.getWorld().getBlockAt(plotLocation).getType() == Material.BEDROCK)
 			            	{
 			            		currentLocation.getBlock().setTypeId(79);
 			            	}
